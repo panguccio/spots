7d90d7de5348a06393328ecb609c28bf2c937b7b
anna <anna.guccione@studenti.units.it>
2026-01-24 19:56:47 +0100

    add database connection setup using MongoDB

---
+++ app/db.js
@@ -0,0 +1,19 @@
+const { MongoClient } = require("mongodb");
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
+
+let db;
+
+async function connect() {
+    if (!db) {
+        try {
+            await client.connect();
+            console.log("Success - Connected to MongoDB");
+            db = client.db("spots");   
+        } catch (error) {
+            console.log("Error - Can't connect to MongoDB");
+            console.error(error)
+        }
+    }
+    return db;
+}
