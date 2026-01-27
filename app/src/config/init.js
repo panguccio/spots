ec210faff90cede66120a4d036238fc2e9a5c9b1
anna <anna.guccione@studenti.units.it>
2026-01-27 10:07:39 +0100

    add config dir and indexed organizerId in tournaments

---
+++ app/src/config/init.js
@@ -0,0 +1,16 @@
+// TODO sistema questo file, dev'essere fatto partire una volta sola all'avvio del db.
+const db = require("./db")
+
+
+async function init() {
+    try {
+        const mongo = await db.connect();
+        await mongo.collection("tournaments").createIndex({ organizerId: 1 });
+        console.log("  * DB init completed");
+    } catch (err) {
+        console.error("  * DB init failed");
+        console.error(err);
+    }
+}
+
+module.exports = init;
