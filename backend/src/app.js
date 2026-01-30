83c452004a424c1d5e045ec1b9cdf8a62f7ea005
anna <anna.guccione@studenti.units.it>
2026-01-30 16:12:00 +0100

    add global error handler

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,8 +6,8 @@ app.use(express.json());
 const cors = require('cors')
 
 app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
+    origin: 'http://localhost:5173',
+    credentials: true
 }))
 
 app.use("/api/auth", require("./routes/auth.js"));
@@ -16,6 +16,12 @@ app.use("/api/tournaments", require("./routes/tournaments.js"));
 app.use("/api/matches", require("./routes/matches.js"));
 app.use("/api/users", require("./routes/users.js"));
 app.use("/api/teams", require("./routes/teams.js"));
+
+app.use((err, req, res, next) => {
+    console.error(err)
+    return res.status(err.status || 500).json({ message: err.message });
+})
+
 
 init().then(() => {
     app.listen(3000, () => {
