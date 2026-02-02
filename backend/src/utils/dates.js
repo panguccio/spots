a282b47d2db215faedc1f6e43935f09cc18631e8
anna <anna.guccione@studenti.units.it>
2026-02-02 09:45:23 +0100

    date fix in backend

--- backend/src/utils/dates.js
+++ backend/src/utils/dates.js
@@ -1,12 +1,11 @@
 function getLimitTimes(date, startHour, endHour) {
-    const start = new Date(date);
-    const end = new Date(date);
+    const [year, month, day] = date.split('-').map(Number);
 
-    const [sh, sm] = startHour.split(":");
-    const [eh, em] = endHour.split(":");
+    const [sh, sm] = startHour.split(":").map(Number);
+    const [eh, em] = endHour.split(":").map(Number);
 
-    start.setHours(sh, sm, 0, 0);
-    end.setHours(eh, em, 0, 0);
+    const start = new Date(year, month - 1, day, sh, sm, 0, 0);
+    const end = new Date(year, month - 1, day, eh, em, 0, 0);
     
     return { end, start };
 }
