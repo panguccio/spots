3fd3b8ded588c7aa8c20de5a3a23672639eece05
anna <anna.guccione@studenti.units.it>
2026-02-02 11:23:07 +0100

    either 0 or 30 minutes

--- backend/src/utils/dates.js
+++ backend/src/utils/dates.js
@@ -1,8 +1,11 @@
 function getLimitTimes(date, startHour, endHour) {
     const [year, month, day] = date.split('-').map(Number);
 
-    const [sh, sm] = startHour.split(":").map(Number);
-    const [eh, em] = endHour.split(":").map(Number);
+    let [sh, sm] = startHour.split(":").map(Number);
+    let [eh, em] = endHour.split(":").map(Number);
+
+    if (sm != 0) sm = 30;
+    if (em != 0) em = 30;
 
     const start = new Date(year, month - 1, day, sh, sm, 0, 0);
     const end = new Date(year, month - 1, day, eh, em, 0, 0);
